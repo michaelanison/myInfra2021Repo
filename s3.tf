3a8fd5e015a1e7e16dcaaa2c2763e0d98574d141
michaelanison <39932682+michaelanison@users.noreply.github.com>
2022-03-29 22:00:35 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "my-jenkins-terraform-bucket" {
+resource "aws_s3_bucket" "my-jenkins-terraform-bucket2" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
   
@@ -7,4 +7,21 @@ resource "aws_s3_bucket" "my-jenkins-terraform-bucket" {
   }
   
   tags = var.tags
+}
+
+
+resource "aws_s3_bucket" "my-jenkins-terraform-bucket2" {
+  bucket_prefix = var.bucket_prefix"
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
