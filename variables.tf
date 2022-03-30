154fd9c7e17acf0ecc659eab1bd5a1bbbf075570
michaelanison <39932682+michaelanison@users.noreply.github.com>
2022-03-29 23:49:09 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -26,21 +26,7 @@ variable "ami_id" {
     description = "AMI for Ubuntu Ec2 instance" 
     default     = "ami-0b0af3577fe5e3532" 
 }
-variable "versioning" {
-    type        = bool
-    description = "(Optional) A state of versioning."
-    default     = true
-}
-variable "acl" {
-    type        = string
-    description = " Defaults to private "
-    default     = "private"
-}
-variable "bucket_prefix" {
-    type        = string
-    description = "(required since we are not using 'bucket') Creates a unique bucket name beginning with the specified prefix"
-    default     = "bucket"
-}
+
 variable "tags" {
     type        = map
     description = "(Optional) A mapping of tags to assign to the bucket."
