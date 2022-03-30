7721adf1dd0523e2463624795d29844951302519
michaelanison <39932682+michaelanison@users.noreply.github.com>
2022-03-29 23:47:53 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,23 +1,15 @@
-#resource "aws_s3_bucket" "bucket" {
-  #bucket_prefix = var.bucket_prefix
-  #acl = var.acl
-  
-  resource "aws_s3_bucket" "this" {
-  count = local.create_bucket ? 1 : 0
+resource "aws_s3_bucket" "my-tf-s3-project1" {
+  bucket = "example-bucket-tf s3-project1"
+}
 
-  bucket        = var.bucket
-  bucket_prefix = var.bucket_prefix
+resource "aws_s3_bucket_acl" "my-tf-s3-project1" {
+  bucket = aws_s3_bucket.my-tf-s3-project1.id
+  acl    = "private"
+}
 
-  # hack when `null` value can't be used (eg, from terragrunt, https://github.com/gruntwork-io/terragrunt/pull/1367)
-  acl = var.acl != "null" ? var.acl : null
-  
-  
+resource "aws_s3_bucket_versioning" "versioning_tf-s3-project1" {
+  bucket = aws_s3_bucket.my-tf-s3-project1.id
+  versioning_configuration {
+    status = "Enabled"
   }
-
-
-   #versioning {
-    #enabled = var.versioning
-  #}
-  
-  #tags = var.tags
-#}
+}
