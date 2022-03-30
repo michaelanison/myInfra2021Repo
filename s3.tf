bbfee6e5422d840fbb63f090a0d74d49fe623c4e
michaelanison <39932682+michaelanison@users.noreply.github.com>
2022-03-29 22:39:16 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,11 +1,10 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket        = var.bucket
+resource "aws_s3_bucket" "my-s3-bucket--" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
-  versioning {
+  
+   versioning {
     enabled = var.versioning
   }
   
   tags = var.tags
 }
-
