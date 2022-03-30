b6762b6215a795bb7942df4348afafc6ac336d87
michaelanison <39932682+michaelanison@users.noreply.github.com>
2022-03-29 23:26:27 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,10 +1,23 @@
-resource "aws_s3_bucket" "bucket" {
+#resource "aws_s3_bucket" "bucket" {
+  #bucket_prefix = var.bucket_prefix
+  #acl = var.acl
+  
+  resource "aws_s3_bucket" "this" {
+  count = local.create_bucket ? 1 : 0
+
+  bucket        = var.bucket
   bucket_prefix = var.bucket_prefix
-  acl = var.acl
+
+  # hack when `null` value can't be used (eg, from terragrunt, https://github.com/gruntwork-io/terragrunt/pull/1367)
+  acl = var.acl != "null" ? var.acl : null
   
-   versioning {
-    enabled = var.versioning
+  
   }
-  
-  tags = var.tags
+
 }
+   #versioning {
+    #enabled = var.versioning
+  #}
+  
+  #tags = var.tags
+#}
